3648468ed8e52f832087ad8b503afa3d59dbdad8
Hanush Kumar S <hanushkumar.su@gmail.com>
2024-08-26 23:21:16 +0530

    updated dotfiles

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.showbreak = "↪ "
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
 require("config.lazy")
